d7de0f232eb03084c687e40eefa82cedc6b86fc4
surmitaj <surmitajais@gmail.com>
2022-07-29 00:07:46 +0530

    Adding files

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="btn"
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
